e95ab037e99c001ded93356da378b4275f4158da
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2024-12-10 08:10:23 +0300

    creating array that stores items

---
+++ index.js
@@ -0,0 +1,10 @@
+const itemInput = document.getElementById('item-input');
+const priorityInput = document.getElementById('priority-input');
+const addItemBtn = document.getElementById('add-item-btn');
+const shoppingList = document.getElementById('shopping-list');
+const clearListBtn = document.getElementById('clear-list-btn');
+const searchInput = document.getElementById('search-input');
+const sortBtn = document.getElementById('sort-btn');
+
+// Array to store shopping list items
+let items = [];
